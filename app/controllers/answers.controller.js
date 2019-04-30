d9cf780e96f1b624bf038873fb3c22182cf3fd18
JOÃO HENRIQUE PINTO CAMPOS <9170491@eseig.pt>
2019-04-30 16:25:08 +0100

    changes

---
+++ app/controllers/answers.controller.js
@@ -0,0 +1,10 @@
+const {Answer}=require("../models/answers.model")
+
+let crudAnswer={
+    //Add Answer
+    addAnswer(idThread,idUser,answer)
+
+    //Add Upvote
+
+    //Remove Upvote
+}
---
+++ app/controllers/comments.controller.js
@@ -0,0 +1,9 @@
+const {Comment}=require("../models/comments.model")
+
+let crudComment={
+    //Add Comment
+
+    //Add Upvote
+
+    //Remove Upvote
+}
