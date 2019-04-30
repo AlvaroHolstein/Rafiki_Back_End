1a89a3a9b38c21c03ebcea61867db58b343f7729
unknown <j_henrique_campos@hotmail.com>
2019-04-30 20:55:17 +0100

    bug fixes and route testing

--- app/models/threads.model.js
+++ app/models/threads.model.js
@@ -1,45 +1,45 @@
-const { Schema, Model } = require('../../mongo_connect')
+const { Schema, Model } = require("../../mongo_connect");
 
-console.log(Schema, Model)
+console.log(Schema, Model);
 
 const threadSchema = Schema({
-    id:{
-        type: Number
-    },
-    userInfo:{
-        userid:Number,
-        photo: String,
-        name: String,
-        rank:String     
-    },  
-    title:{
-        type: String
-    },
-    question:{
-        type: String
-    },
-    tags:[{
-        id:Number,
-        text:String
-    }],
-    upvotes:{
-        type: Number
-    },
-    date:{
-        type:Date
-    },
-    closeDate:{
-        type: Date,
-        default: null
-    },
-    views:{
-        type: Number
-    }
-})
+  id: {
+    type: Number
+  },
+  userInfo: {
+    userid: Number,
+    photo: String,
+    name: String,
+    rank: String
+  },
+  title: {
+    type: String
+  },
+  question: {
+    type: String
+  },
+  tags: {
+    type: Array,
+    default: []
+  },
+  upvotes: {
+    type: Number
+  },
+  date: {
+    type: Date
+  },
+  closeDate: {
+    type: Date,
+    default: null
+  },
+  views: {
+    type: Number
+  }
+});
 
-const Thread= Model("Thread",threadSchema)
+const Thread = Model("Thread", threadSchema);
 
-module.exports={
-    Thread:Thread,
-    threadSchema: threadSchema
-}
+module.exports = {
+  Thread: Thread,
+  threadSchema: threadSchema
+};
--- server.js
+++ server.js
@@ -1,40 +1,51 @@
-const express = require('express')
-const bodyParser = require('body-parser')
-const session = require('express-session')
-const cors = require('cors')
-require('dotenv').config()
-const userController = require('./app/controllers/users.controller')
-const badgeController = require('./app/controllers/badges.controller')
-const threadController = require('./app/controllers/threads.controller')
+const express = require("express");
+const bodyParser = require("body-parser");
+const session = require("express-session");
+const cors = require("cors");
+require("dotenv").config();
+const userController = require("./app/controllers/users.controller");
+const badgeController = require("./app/controllers/badges.controller");
+const threadController = require("./app/controllers/threads.controller");
+const tagController = require("./app/controllers/tags.controller");
+const app = express();
 
-const app = express()
+const port = process.env.PORT || 80;
 
-const port = process.env.PORT || 80
-
-app.use(cors())
+app.use(cors());
 /**
- * Fazer rotas 
+ * Fazer rotas
  */
 
-app.get('/', (req, res) => {
-    res.send('Back End Rafiki')
-})
+app.get("/", (req, res) => {
+  res.send("Back End Rafiki");
+});
 
-app.get('/allusers', (req, res) => {
-    //Função criada no controller
-    userController.findAll(res)
-})
-app.get('/allthreads', (req, res) => {
-    threadController.findAll(res)
-})
+app.get("/allusers", (req, res) => {
+  //Função criada no controller
+  userController.findAll(res);
+});
 
-app.get('/allbadges', (req, res) => {
-    //badges.controller
-    badgeController.findAll(res)
-})
+app.get("/allbadges", (req, res) => {
+  //badges.controller
+  badgeController.findAll(res);
+});
+//Thread.controller
+app.get("/allthreads", (req, res) => {
+  threadController.findAll(res);
+});
+app.get("/findTag", (req, res) => {
+  threadController.findByTag(res, ["Vue.js"]);
+});
+app.get("/findkeyword", (req, res) => {
+  threadController.findByKeyword(res, "Thread");
+});
+//Tag.controller
+app.get("/allTags", (req, res) => {
+  tagController.findAll(res);
+});
 
 app.listen(port, () => {
-    console.log(`Server running on port :${port}`)
-})
+  console.log(`Server running on port :${port}`);
+});
 
-module.exports = app; 
+module.exports = app;
