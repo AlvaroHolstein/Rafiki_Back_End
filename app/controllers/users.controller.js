6fec54a1d0ea891e46ee31b1850b109d6745c322
alvaro <adholstein@gmail.com>
2019-04-30 15:00:16 +0100

    change in mongo connection

--- app/controllers/users.controller.js
+++ app/controllers/users.controller.js
@@ -7,15 +7,25 @@ let crudUser = {
                 console.log(err, 'erro')
             }
             else {
+                console.log(collection)
                 res.json(collection)
             }
         })
     },
     findOneByName(res, name) {
-
+        //Encontrar o user e devolve-lo
     },
     findOneByEmail(res, mail) {
-        
+        //Encontrar user e devolver-lo
+    },
+    updateUser(res, user) {
+        //Encontrar user e atualizar
+    },
+    deleteUser(res, id) {
+        // Encontrar user, eliminá-lo e devolver este user
+    },
+    insertUser(res, user) {
+
     }
 }
 
