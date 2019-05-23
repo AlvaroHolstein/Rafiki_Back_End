a7e2cb580e59637c22e9c1a9ee7c8d0819d498fa
unknown <j_henrique_campos@hotmail.com>
2019-05-23 17:56:50 +0100

    forgotPassword

--- app/controllers/auth/auth.controller.js
+++ app/controllers/auth/auth.controller.js
@@ -11,16 +11,22 @@ let Auth = {
     User.find({}, (err, collection) => {
       if (err) throw err;
       if (collection.length != 0) {
-        collection.sort(function (a, b) {
+        collection.sort(function(a, b) {
           if (a.id > b.id) return 1;
           if (a.id < b.id) return -1;
         });
         id = collection[collection.length - 1].id + 1;
-        console.log(req.body, 'req.body')
+        console.log(req.body, "req.body");
       }
 
-      if (req.body.name != undefined && !collection.some(user => user.name === req.body.name)) {
-        if (req.body.email != undefined && !collection.some(user => user.email === req.body.email)) {
+      if (
+        req.body.name != undefined &&
+        !collection.some(user => user.name === req.body.name)
+      ) {
+        if (
+          req.body.email != undefined &&
+          !collection.some(user => user.email === req.body.email)
+        ) {
           if (req.body.password != undefined && req.body.password != "") {
             var hashedPassword = bcrypt.hashSync(req.body.password, 8);
             //REgister
@@ -31,7 +37,7 @@ let Auth = {
                 email: req.body.email,
                 password: hashedPassword
               },
-              function (err, user) {
+              function(err, user) {
                 if (err)
                   return res
                     .status(500)
@@ -43,8 +49,8 @@ let Auth = {
                 res.status(200).send({ auth: true, token: token });
               }
             );
-          } else res.send('Password Inválida')
-        } else res.send('email inválido') 
+          } else res.send("Password Inválida");
+        } else res.send("email inválido");
       } else {
         res.send("Name Already Exists");
       }
@@ -52,8 +58,8 @@ let Auth = {
   },
   //login
   login(req, res) {
-    console.log(req.body, 'req.body no login')
-    User.findOne({ email: req.body.email }, function (err, user) {
+    console.log(req.body, "req.body no login");
+    User.findOne({ email: req.body.email }, function(err, user) {
       if (err) return res.status(500).send("Error on the server.");
       if (!user) return res.status(404).send("No user found.");
       var passwordIsValid = bcrypt.compareSync(
@@ -65,13 +71,105 @@ let Auth = {
       var token = jwt.sign({ id: user._id }, secret, {
         expiresIn: "1h" // expires in 1 hour
       });
-      res.cookie('se_la_cookie', 'lelele', {maxAge: 90000, httpOnly: false, secure: false})
+      res.cookie("se_la_cookie", "lelele", {
+        maxAge: 90000,
+        httpOnly: false,
+        secure: false
+      });
       res.status(200).send({ auth: true, token: token, id: user.id });
     });
   },
   //logout
   logout(req, res) {
     res.status(200).send({ auth: false, token: null });
+  },
+  passwordreset(req, res) {
+    if (req.body.email !== undefined) {
+      var emailAddress = req.body.email;
+
+      User.findOne({ email: emailAddress }, function(err, user) {
+        if (err) return res.status(500).send("Error on the server");
+        if (!user) return res.status(404).send("No user with this email");
+        var payload = {
+          id: user.id,
+          email: user.email
+        };
+        var secret = process.env.SECRET + user.password;
+
+        var token = jwt.sign(payload, secret);
+
+        // TODO: Send email containing link to reset password.
+        // In our case, will just return a link to click.
+        res.send(
+          '<a href="/auth-api/resetpassword/' +
+            user.id +
+            "/" +
+            token +
+            '">Reset password</a>'
+        );
+      });
+    } else {
+      res.send("Email address is missing.");
+    }
+  },
+  resetpassword(req, res) {
+    // TODO: Fetch user from database using
+    // req.params.id
+    // TODO: Decrypt one-time-use token using the user's
+    // current password hash from the database and combine it
+    // with the user's created date to make a very unique secret key!
+    // For example,
+    // var secret = user.password + ‘-' + user.created.getTime();
+    User.find({ id: req.params.id }, (err, user) => {
+      if (err) throw err;
+      var secret = process.env.SECRET + user.password;
+      var payload = jwt.decode(req.params.token, secret);
+      // TODO: Gracefully handle decoding issues.
+      // Create form to reset password.
+      res.send(
+        '<form action="/auth-api/resetpassword" method="POST">' +
+          '<input type="hidden" name="id" value="' +
+          payload.id +
+          '" />' +
+          '<input type="hidden" name="token" value="' +
+          req.params.token +
+          '" />' +
+          '<input type="password" name="password" value="" placeholder="Enter your new password..." />' +
+          '<input type="submit" value="Reset Password" />' +
+          "</form>"
+      );
+    });
+  },
+  changepassword(req, res) {
+    console.log(req.body);
+    // TODO: Fetch user from database using
+    // req.body.id
+    // TODO: Decrypt one-time-use token using the user's
+    // current password hash from the database and combining it
+    // with the user's created date to make a very unique secret key!
+    // For example,
+    // var secret = user.password + ‘-' + user.created.getTime();
+    User.find({ id: req.body.id }, (err, user) => {
+      if (err) throw err;
+      var secret = process.env.SECRET + user.password;
+
+      var payload = jwt.decode(req.body.token, secret);
+      console.log(payload);
+
+      // TODO: Gracefully handle decoding issues.
+      // TODO: Hash password from
+      // req.body.password
+      var hashedPassword = bcrypt.hashSync(req.body.password, 8);
+      User.findOneAndUpdate(
+        { id: req.body.id },
+        { password: hashedPassword },
+        function(err, user) {
+          if (err) throw err;
+          console.log(user);
+        }
+      );
+      res.send("Your password has been successfully changed.");
+    });
   }
 };
 
--- app/routes/authentication.route.js
+++ app/routes/authentication.route.js
@@ -1,22 +1,42 @@
-const router = require('express').Router()
-const authController = require('../controllers/auth/auth.controller')
-const verifyToken = require('../controllers/auth/VerifyToken')
+const router = require("express").Router();
+const authController = require("../controllers/auth/auth.controller");
+const verifyToken = require("../controllers/auth/VerifyToken");
 
-router.get('/', function (req, res) {
-    console.log(req);
-    res.send('<h1>Routa para "chamadas" de autenticação</h1>')
-})
+router.get("/", function(req, res) {
+  console.log(req);
+  res.send('<h1>Routa para "chamadas" de autenticação</h1>');
+});
 
 //Authentication
 router.post("/register", (req, res) => {
-    authController.register(req, res);
+  authController.register(req, res);
 });
 router.post("/login", (req, res) => {
-    authController.login(req, res);
+  authController.login(req, res);
 });
 
 router.get("/logout", verifyToken, (req, res) => {
-    authController.logout(req, res);
+  authController.logout(req, res);
 });
 
+router.get("/forgotpassword", function(req, res) {
+  res.send(
+    '<form action="/auth-api/passwordreset" method="POST">' +
+      '<input type="email" name="email" value="" placeholder="Enter your email address..." />' +
+      '<input type="submit" value="Reset Password" />' +
+      "</form>"
+  );
+});
+
+router.post("/passwordreset", function(req, res) {
+  authController.passwordreset(req, res);
+});
+
+router.get("/resetpassword/:id/:token", function(req, res) {
+  authController.resetpassword(req, res);
+});
+
+router.post("/resetpassword", function(req, res) {
+  authController.changepassword(req, res);
+});
 module.exports = router;
